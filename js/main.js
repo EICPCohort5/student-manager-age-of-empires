1d726817f2d751728d3b53bf492e510b88aa3bb1
Daniel Kotlinski <dan61160@tjx.com>
2022-02-24 11:06:45 -0500

    update main.js and index.html

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 // Your main JavaScript code goes here
 // This is set up as an ECMAScript module, so you can import other ES Modules
+
+// students have, at a minimum, a firstName, lastName, and location (US, UK, Canada), modeling the students in the bootcamp
