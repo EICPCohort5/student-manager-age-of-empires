f91ae700d9df83147af36875b0fbb6267cb6ed2b
Daniel Kotlinski <dan61160@tjx.com>
2022-02-24 11:27:55 -0500

    working student params

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-// Your main JavaScript code goes here
-// This is set up as an ECMAScript module, so you can import other ES Modules
-
 // students have, at a minimum, a firstName, lastName, and location (US, UK, Canada), modeling the students in the bootcamp
+const form = document.querySelector('#student-app');
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event){
+  event.preventDefault();
+
+  let studentData = new FormData(form);
+  iteratewithentriesDestruct(studentData);
+
+}
+
+
+function iteratewithentriesDestruct(studentData){
+  for (let [key,value] of studentData.entries()){
+    console.log(`${key}: ${value}`);
+  }
+}
+
+
+
+
+
+//export {};
